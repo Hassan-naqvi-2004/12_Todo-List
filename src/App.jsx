e4d67877544ed2072407be64c9e1284f73ff4b18
Hassan-naqvi-2004 <thehassanabbasnaqvi@gmail.com>
2024-03-15 14:10:27 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,31 @@ import { IoMdAdd } from "react-icons/io";
 const App = () => {
 
   const [todo, setTodo] = useState([]);
+  const [mycolor,setMycolor]=useState('')
+  
   const ref = useRef(null);
   console.log(ref);
 
+  // const handleClick = ()=>{
+  //   setTodo([...todo,{text:ref.current.value,color:mycolor}])
+  //   ref.current.value=''
+  // }
+
   const handleClick = ()=>{
-    setTodo([...todo,ref.current.value])
-    ref.current.value=''
+      setTodo([... todo,{text:ref.current.value,color:mycolor}])
+      ref.current.value=''
   }
 
   const handleDel = (item)=>{
     setTodo(todo.filter((e)=> item !== e ))
+  }
+  const themeColor = (col)=>{
+    setMycolor(col)
+
+  }
+  const bg = {
+    backgroundColor:mycolor,
+   
   }
   return (
     <div>
@@ -26,6 +41,12 @@ const App = () => {
       <nav> 
       <div className='navbar'>
         <h1>TODO LIST <ImPower /></h1>
+        <div className="buttons">
+          <button className='green' onClick={()=> themeColor('green')}></button>
+          <button className='blue' onClick={()=> themeColor('rgb(170, 170, 199)')}></button>
+          <button className='yellow' onClick={()=>themeColor('yellow')}></button>
+
+        </div>
       <div className='btn'>  
       <input type="text" ref={ref}/>
       <button onClick={handleClick} ><IoMdAdd /></button>
@@ -37,8 +58,8 @@ const App = () => {
       {todo.map((item,i)=>{
       return(
         <>
-        <div className='ap'>
-        <p>{`${i+1}:${item}`}</p>
+        <div className='ap' style={bg}>
+        <p>{`${i+1}:${item.text}`}</p>
         <button onClick={()=>handleDel(item)}>delete</button>
         </div>
         </>
